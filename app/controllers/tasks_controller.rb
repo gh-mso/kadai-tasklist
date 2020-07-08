497ac823afadfebe1a6648da115ce1708685a251
“mei” <meirinmeiri@gmail.com>
2020-07-08 14:39:12 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:new, :edit, :show]
+  before_action :require_user_logged_in, only: [:new, :show, :edit]
+  before_action :correct_user, only: [:edit, :show, :destroy]
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks
+    end
   end
 
   def show
@@ -13,7 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
@@ -51,8 +55,15 @@ class TasksController < ApplicationController
   private
 
   # Strong Parameter
-    def task_params
-      params.require(:task).permit(:content, :status)
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+    
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
     end
+  end
 end
 
